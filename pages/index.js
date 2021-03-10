cca125db95effc1a0ffc692f76b2de42c3e83f74
kim0612 <roki03@naver.com>
2021-03-10 15:57:56 +0900

    #5.1
    Static Generation
    index page에 적용.

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import { Divider, Header, Loader } from "semantic-ui-react";
 import ItemList from "../src/components/ItemList";
 
-export default function Home() {
+export default function Home({list}) {
+/*
   const [list, setList] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   
@@ -22,29 +23,33 @@ export default function Home() {
   }
 
   useEffect(getData,[]);
-
+*/
   return (
-    <div>
+    <>
       <Head>
         <title>Home | KIM0612</title>
         <meta name="description" content="KIM0612 홈 입니다."></meta>
       </Head>
-      {isLoading
-        ? 
-          <div style={{paddingTop:"200px"}}>
-           <Loader inline="centered" active>Loading</Loader>
-          </div>
-        :
-          <>
-            <Header as="h2" style={{paddingTop: "40px"}}>베스트상품</Header>
-            <Divider/>
-            <ItemList list={list.slice(0,9)}/>
 
-            <Header as="h2" style={{paddingTop: "40px"}}>신상품</Header>
-            <Divider/>
-            <ItemList list={list.slice(9,)}/>
-          </>
-      }
-    </div>
+      <Header as="h2" style={{paddingTop: "40px"}}>베스트상품</Header>
+      <Divider/>
+      <ItemList list={list.slice(0,9)}/>
+
+      <Header as="h2" style={{paddingTop: "40px"}}>신상품</Header>
+      <Divider/>
+      <ItemList list={list.slice(9,)}/>
+    </>
   )
+}
+
+export const getStaticProps = async () => {
+  const apiUrl = process.env.apiUrl;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return {
+    props:{
+      list:data
+    }
+  };
 }
